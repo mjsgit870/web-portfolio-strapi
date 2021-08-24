74e7c58bd20bd95f4ed5143d05a8839754e9427a
unknown <saputraj870@gmail.com>
2021-08-24 20:14:03 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c853ac7b6bcfd1a7dfe79979489cd39b'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a3503acf-cd01-4912-a570-d189e1e04a3c'
+};
